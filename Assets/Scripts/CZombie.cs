01dd7b07211f06f5afd7c21ab0e2f290af8bffa5
Nick R <nro1912@gmail.com>
2021-06-05 17:50:47 +0300

    First try

---
+++ Assets/Scripts/CZombie.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CZombie : MonoBehaviour
+{
+    public Animator m_animator;
+    public Transform m_transform;
+    public  bool isWalk;
+    public bool isRun;
+    public Vector3 vForward;
+    private CMove move;
+    private CTimer zwait;
+    public float walkSpeed;
+    public float runSpeed;
+    public int steps = 8;
+    public int curStep = 0;
+    public int a = 360;
+
+    ////////////////////////////////////////////////
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_transform = GetComponent<Transform>();
+        walkSpeed = 0.5f;
+        runSpeed = 2.0f;
+        isWalk = false;
+        isRun = false;
+        move = new CMove();
+        zwait = new CTimer();
+        move.SetActionSpeed(walkSpeed);
+        vForward = m_transform.forward;
+        zwait.SetActionTime(3);
+    }
+
+    private void StartWalk()
+    {
+        isWalk = true;
+        move.SetPositions(m_transform.position, m_transform.position + vForward);
+        move.StartAction();
+        m_animator.SetBool("walk", isWalk);
+   }
+
+    private void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isWalk)
+        {
+            move.SetActionSpeed(walkSpeed);
+            StartWalk();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (isWalk) isRun = true;
+            // m_animator.SetBool("run", isRun);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            if (isRun) isRun = false;
+            m_animator.SetBool("run", isRun);
+        }
+    }
+
+    //////////////////////////////////
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        if (move.IsActive())
+        {
+            if (!move.UpdatePosition())
+            {
+                isWalk = false;
+            }
+            m_transform.position = move.GetCurrentPosition();
+            if (!isWalk)
+            {
+                if (++curStep < steps)
+                {
+                    if (isRun)
+                    {
+                        move.SetActionSpeed(runSpeed);
+                        m_animator.SetBool("run", isRun);
+                    }
+                    else move.SetActionSpeed(walkSpeed);
+                    StartWalk();
+                }
+                else
+                {
+                    curStep = 0;
+                    m_animator.SetBool("walk", isWalk);
+                    zwait.StartAction();
+                    isRun = false;
+                    m_animator.SetBool("run", isRun);
+                    m_animator.SetBool("attack", true);
+                }
+            }
+        }
+        if (zwait.IsActive())
+        {
+            if (zwait.UpdateState())
+            {
+             float angle;
+
+                angle = a - 90.0f * zwait.GetState();
+                m_transform.rotation = Quaternion.Euler(0, angle, 0);
+            }
+            else
+            {
+                a -= 90;
+                m_transform.rotation = Quaternion.Euler(0, a, 0);
+                if (a == 0) a = 360;
+                vForward = m_transform.forward;
+                m_animator.SetBool("attack", false);
+                move.SetActionSpeed(walkSpeed);
+                StartWalk();
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Transform m_transform;
+    private Transform cam_transform;
+    private int n;
+    private int selectedPosition;
+    private Vector3 pos;
+    public Vector3[] posList;
+    private const int maxPos = 5;
+    public Camera m_camera;
+//    public Vector3 posCamera;
+    private CMove moveCam;
+    private CMove moveCube;
+
+    ///////////////////
+    // Start
+    void Start()
+    {
+        m_transform = GetComponent<Transform>();
+        cam_transform = m_camera.GetComponent<Transform>();
+        cam_transform.LookAt(m_transform.position);
+        n = 0;
+        selectedPosition = 1;
+//        posCamera = new Vector3(0, 5, -10);
+        posList = new Vector3[maxPos];
+        posList[0] = new Vector3(-3, 3.5f, 2);
+        posList[1] = new Vector3(0, 1, 0);
+        posList[2] = new Vector3(0, 3, 0);
+        posList[3] = new Vector3(2, 3, -1);
+        posList[4] = new Vector3(2, 2, 2);
+        pos = posList[selectedPosition];
+        moveCam = new CMove();
+        moveCube = new CMove();
+        moveCam.SetActionTime(2.0f);
+        moveCube.SetActionSpeed(2.0f);
+    }
+    
+    void FixedUpdate()
+    {
+        if (++n == 360)
+            n = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab) && !moveCube.IsActive())
+        {
+            if (++selectedPosition == maxPos) selectedPosition = 0;
+            pos = posList[selectedPosition];
+            if (m_transform.position != pos)
+            {
+                moveCube.SetPositions(m_transform.position, pos);
+                moveCube.StartAction();
+                moveCam.SetPositions(m_transform.position, pos);
+//              moveCam.SetPositions(cam_transform.position, pos + posCamera);
+                moveCam.StartAction();
+                //m_transform.position = pos;
+            }
+        }
+        if (moveCube.IsActive())
+        {
+            moveCube.UpdatePosition();
+            m_transform.position = moveCube.GetCurrentPosition();
+        }
+        m_transform.rotation = Quaternion.Euler(n, n, 0);
+    }
+
+    void LateUpdate()
+    {
+        if (moveCam.IsActive())
+        {
+            moveCam.UpdatePosition();
+//            cam_transform.position = moveCam.GetCurrentPosition();
+            cam_transform.LookAt( moveCam.GetCurrentPosition() );
+        }
+    }
+
+}
